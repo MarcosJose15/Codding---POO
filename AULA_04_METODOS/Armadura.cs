774bad7d9215f3d9377b77215b7425418fa51d65
Marcos José <87348965+MarcosJose15@users.noreply.github.com>
2022-09-06 17:45:14 -0300

    AULA 04

---
+++ AULA_04_METODOS/Armadura.cs
@@ -0,0 +1,6 @@
+public class Armadura
+{
+    public string nome;
+    public string descricao;
+    public int pontuacao;
+}
---
+++ AULA_04_METODOS/Program.cs
@@ -0,0 +1,20 @@
+﻿Personagem gerald = new Personagem();
+gerald.nome = "Gerald";
+gerald.classe = "Bruxo";
+gerald.vida = 10;
+
+gerald.armadura = new Armadura();
+gerald.armadura.nome = "Armadura da escola do lobo";
+gerald.armadura.pontuacao = 10;
+gerald.armadura.descricao = "Armadura do cachorrão";
+
+Personagem patolino = new Personagem();
+patolino.nome = "Patolino";
+patolino.classe = "Mago";
+patolino.vida = 10;
+
+for (int i = 0; i < 5; i++)
+{
+    gerald.Lutar(patolino);
+    patolino.Lutar(gerald);
+}
