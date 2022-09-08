80832d6ef9164a56e179f473023245b7c52f2713
Marcos José <87348965+MarcosJose15@users.noreply.github.com>
2022-09-08 16:31:21 -0300

    part 2

---
+++ Bebida.cs
@@ -0,0 +1,8 @@
+public class Bebida
+{
+    public string nome;
+    public string marca;
+    public int preco;
+    public string validade;
+    public string dataFabricacao;
+}
---
+++ Consulta.cs
@@ -0,0 +1,9 @@
+public class Consulta
+{
+    public string nomeMedico;
+    public string nomePaciente;
+    public string horario;
+    public string hospital;
+    public string tempoAtendimento;
+    public string exames;
+}
