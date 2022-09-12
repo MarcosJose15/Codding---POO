98bd0e53187c439e205c44bf363c0b3d7e78afe1
Marcos José <87348965+MarcosJose15@users.noreply.github.com>
2022-09-12 17:50:38 -0300

    Delete Consulta.cs

--- Consulta.cs
+++
@@ -1,9 +0,0 @@
-public class Consulta
-{
-    public string nomeMedico;
-    public string nomePaciente;
-    public string horario;
-    public string hospital;
-    public string tempoAtendimento;
-    public string exames;
-}
