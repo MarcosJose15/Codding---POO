c71247669f168e2be190413e8be20c27b9ce93aa
Marcos José <87348965+MarcosJose15@users.noreply.github.com>
2022-09-12 17:50:45 -0300

    Delete Funcionario.cs

--- Funcionario.cs
+++
@@ -1,9 +0,0 @@
-public class Funcionario
-{
-    public string nome;
-    public string dataNasc;
-    public string setor;
-    public string cargo;
-    public string endereco;
-    public float salario;
-}
